1cc805f42871f44f402ecb8888fb738d93eb03e4
Jailon Lawrence <jailon706@icloud.com>
2024-01-18 13:42:58 -0600

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ApplicationExample 01182024"
+include(":app")
+ 
